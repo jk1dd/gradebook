7dd76f2c1f8271ae5ceb65638e3ff71d6153666e
jk1dd <portajonk@gmail.com>
2017-08-24 07:50:18 -0600

    allow teacher to see student, not grades

--- spec/features/teacher_can_see_students_spec.rb
+++ spec/features/teacher_can_see_students_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature 'Teacher can see students' do
     teacher = create(:teacher)
     course = create(:course, name: 'Music', teacher: teacher)
     course.students.create(name: 'Joanna')
+    
     course.students.create(name: 'Steve')
     # student_1, student_2, student_3 = create_list(:student, 3, course: course)
 
